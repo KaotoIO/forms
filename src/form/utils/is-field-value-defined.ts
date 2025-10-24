6644c77b36e72501e6d56ba091313360c15c1be2
Shivam Gupta <shvmgpt640@gmail.com>
2025-10-25 01:13:04 +0530

    Fix(modified_fields): Updated empty text fields falsely appears under modified tab

---
+++ src/form/utils/is-field-value-defined.ts
@@ -0,0 +1,35 @@
+import { JSONSchema4TypeName } from 'json-schema';
+import { isDefined } from './is-defined';
+
+export const isFieldValueDefined = (
+  schemaType: JSONSchema4TypeName[] | JSONSchema4TypeName | undefined,
+  value: unknown,
+): boolean => {
+  if (!isDefined(schemaType)) return false;
+
+  let isFieldDefined = false;
+  // When field of any type in not defined with any input, the `value` returns `undefined` and therefore we return false
+  if (isDefined(value)) {
+    // Check if the value is a valid value
+    switch (schemaType) {
+      case 'object':
+        isFieldDefined = typeof value === 'object' && Object.keys(value).length > 0;
+        break;
+      case 'array':
+        isFieldDefined = Array.isArray(value) && value.length > 0;
+        break;
+      case 'boolean':
+        // when a boolean with default value (false or true) is defined, its value remains undefined until user changes it
+        isFieldDefined = typeof value === 'boolean';
+        break;
+      case 'string':
+      case 'number':
+        // when a string or number is defined with a value and then cleared, it becomes an empty string
+        isFieldDefined = value !== '';
+        break;
+      default:
+        isFieldDefined = true;
+    }
+  }
+  return isFieldDefined;
+};
